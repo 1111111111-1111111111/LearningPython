9afa073737502b97fd46f3c244ea0c552cb13407
木子杳杉 <15422155+muzi-yaoshan@user.noreply.gitee.com>
2025-07-31 14:48:54 +0800

    2025-7-31

---
+++ python笔记+代码/day01-基本数据类型与变量/代码/demo02.py
@@ -0,0 +1,43 @@
+# name-->变量名
+name = "老王*100000"
+age = 40
+print("老王")
+print("name")
+print(name, age)
+
+# 多重赋值
+mingzi, age, sex = "老王", 40, "男"
+print(mingzi, age, sex)
+
+# 链式赋值:把同一个值赋值给多个变量名
+a = b = c = 5
+print(a, b, c)
+
+# 交叉赋值：将两个变量指向的值进行交换
+m = 1
+n = 2
+print(m, n)
+m, n = n, m
+print(m, n)
+
+# 在 Python 中，变量名可以由字母、下划线和数字组成，但**变量名的第一个字符不能是数字**。
+# 正确的命名
+name = "老王"
+name_01 = "老王"
+_name = "老王"
+name01 = "老王"
+# 19name = "老王"
+
+name = "老王"
+Name = "小明"
+print(name, Name)
+
+
+
+
+
+
+
+
+
+
